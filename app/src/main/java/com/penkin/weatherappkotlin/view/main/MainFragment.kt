f023e94ef2b02f642d48aa009054097875e29c09
Oleg <49286750+Daganep@users.noreply.github.com>
2021-03-21 07:30:56 +0300

    MainFragment init.

--- app/src/main/java/com/penkin/weatherappkotlin/view/main/MainFragment.kt
+++ app/src/main/java/com/penkin/weatherappkotlin/view/main/MainFragment.kt
@@ -1,4 +1,28 @@
 package com.penkin.weatherappkotlin.view.main
 
-class MainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.penkin.weatherappkotlin.databinding.FragmentMainBinding
+import moxy.MvpAppCompatFragment
+
+class MainFragment: MvpAppCompatFragment(), MainView {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
