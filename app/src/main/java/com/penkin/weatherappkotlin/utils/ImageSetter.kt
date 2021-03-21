8987b52deeed8fe37a69e6e62f247e899278b0c1
Oleg <49286750+Daganep@users.noreply.github.com>
2021-03-21 17:42:16 +0300

    Picasso init.

--- app/src/main/java/com/penkin/weatherappkotlin/utils/ImageSetter.kt
+++ app/src/main/java/com/penkin/weatherappkotlin/utils/ImageSetter.kt
@@ -1,4 +1,13 @@
 package com.penkin.weatherappkotlin.utils
 
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
 class ImageSetter {
+    fun setImage(url: String?, imageView: ImageView){
+        Picasso
+                .get()
+                .load(url)
+                .into(imageView)
+    }
 }
